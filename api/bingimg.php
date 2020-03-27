cd09a29206e58c2d2aa3bb2c74c35d632bad31e0
王嘉麟 <47003490+wjlfish@users.noreply.github.com>
2020-03-27 23:36:22 +0800

    Update bingimg.php

--- api/bingimg.php
+++ api/bingimg.php
@@ -1,3 +1,5 @@
+/*这是本站的一个api接口，外部可以通过https://www.wjlnb.com/api/bingimg请求调用
+api内容：bing主页每日一图，使用其原生api中的信息，获取图片地址，目前被用于网站主页的背景图*/
 <?php
 $date = mt_rand(0, 7); //7天图片
 $json_content = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=' . $date . '&n=1&mkt=zh-CN'); //读取必应api，获得相应数据
